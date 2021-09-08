8165403defef345a4367e9e04531f0f9d68dc117
Michael Leshchenko <mleshchenko@VRP.local>
2021-09-08 13:39:44 +0300

    repair invoke modal lwc from product page

--- force-app/main/default/lwc/modalWindowForm/modalWindowForm.js
+++ force-app/main/default/lwc/modalWindowForm/modalWindowForm.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ModalWindowForm extends LightningElement {}
+export default class ModalWindowForm extends LightningElement {
+    @api productid
+}
