ad93b1275dce5610f7fd11e1982889c2629e9314
Michael Leshchenko <mleshchenko@VRP.local>
2021-09-09 15:40:00 +0300

    created modal window. Customer_history save in db

--- force-app/main/default/lwc/modalWindowForm/modalWindowForm.js
+++ force-app/main/default/lwc/modalWindowForm/modalWindowForm.js
@@ -1,5 +1,26 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import CUSTOMER_HISTORY_OBJ from '@salesforce/schema/Customer_history__c';
+
+import NAME_FIELD from '@salesforce/schema/Customer_history__c.Name';
+import MOBILE_PHONE_FIELD from '@salesforce/schema/Customer_history__c.Mobile_phone__c';
+// import PRODUCT_FIELD from '@salesforce/schema/Customer_history__c.Product__c';
+import QUANTITY_FIELD from '@salesforce/schema/Customer_history__c.Quantity__c';
 
 export default class ModalWindowForm extends LightningElement {
-    @api productid
+    @api productid;
+    @api  objectApiName = CUSTOMER_HISTORY_OBJ
+
+    fields = [NAME_FIELD, MOBILE_PHONE_FIELD, QUANTITY_FIELD];
+
+
+    handleSubmit(event){
+        event.preventDefault();
+        const fields = event.detail.fields;
+        this.template.querySelector('lightning-record-form').submit(fields);
+    }
+
+    handleSuccess(event){
+        this.template.querySelector(".message").innerText = 'Your order submitted';
+    }
 }
+
--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -9,12 +9,18 @@ export default class productCard extends LightningElement {
     @wire(CurrentPageReference)
     setRecordId(currentPageReference) {
         this.recordId = currentPageReference.state.id;
+        if (this.recordId == null)
+        {
+            this.recordId = '01t5g0000045eFNAAY';
+        };
     }
 
     @wire(getDataForProductCard, {Id: '$recordId'}) productData;
 
     showPopUpForm(){
+        console.log('popup show ' + this.showModalWindowForm );
         this.showModalWindowForm = true;
+        console.log('popup show ' + this.showModalWindowForm );
     }
 
 }
