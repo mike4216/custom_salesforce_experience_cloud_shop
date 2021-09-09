2de12bce341b01a3be85b6ddfdef2b404f5ed8d0
Michael Leshchenko <mleshchenko@VRP.local>
2021-09-09 19:31:08 +0300

    created pagination

--- force-app/main/default/lwc/gallery/gallery.js
+++ force-app/main/default/lwc/gallery/gallery.js
@@ -1,7 +1,49 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, track, wire } from "lwc";
 import getDataForGallery from "@salesforce/apex/ShopController.getDataForGallery";
 
 export default class gallery extends LightningElement {
-    @wire(getDataForGallery) products;
+    allProducts;
+    numberProductsPerPage = 10;
+    currentPageNumber = 1;
+    @track  totalProducts;
+    @track showFromTotal = this.numberProductsPerPage;
+    @track products;
+
+
+    @wire(getDataForGallery)
+    getAllProducts(response){
+        if(response.data){
+            this.allProducts = response.data;
+            this.products = this.allProducts.slice(0,this.numberProductsPerPage);
+            this.totalProducts = this.allProducts.length;
+        }else if (response.error){
+            //handle
+        }
+    }
    
+    paginateNext(){
+        var previousProducts = this.currentPageNumber * this.numberProductsPerPage
+        this.products = this.allProducts.slice(
+            previousProducts,
+            previousProducts + this.numberProductsPerPage
+        );
+        this.showFromTotal = previousProducts + this.numberProductsPerPage
+        this.currentPageNumber += 1;
+    }
+
+    paginatePrev(){
+        if(this.currentPageNumber == 1) return;
+
+        var prevProducts = this.currentPageNumber * this.numberProductsPerPage;
+        this.products = this.allProducts.slice(
+            prevProducts - this.numberProductsPerPage * 2,
+            prevProducts - this.numberProductsPerPage   
+        );
+        this.showFromTotal = prevProducts - this.numberProductsPerPage;
+        this.currentPageNumber -= 1;
+
+    }
+
+
+    
 }
