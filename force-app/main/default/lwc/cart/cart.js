3d56bbe225fa45afe06929b030e9b7bf4915b0d1
Michael Leshchenko <michael.leshchenko@vrpconsulting.com>
2021-10-01 13:30:37 +0300

    added show products logic in cart

--- force-app/main/default/lwc/cart/cart.js
+++ force-app/main/default/lwc/cart/cart.js
@@ -1,21 +1,12 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import getDataForProductCard from '@salesforce/apex/ShopController.getDataForProductCard';
+import getDataForCart from '@salesforce/apex/ShopController.getDataForCart';
 
 
 export default class Cart extends LightningElement {
-    cookiesArr;
-
-    constructor(){
-        super();
-        this.cookiesArr = this.readCookie();
-    }
-    
-
-    //получить из куки массив id
-    //рассчиатвть counter при загрузке страницы
-    // 
+    @track products
 
     showCartWindow(){
+        this.handleLoad();
         var data = this.template.querySelector(".slds-is-open");
         if (data){
             this.template.querySelector(".slds-dropdown-trigger").classList.remove('slds-is-open');
@@ -24,9 +15,10 @@ export default class Cart extends LightningElement {
         }
     }
 
-
-    @wire(getDataForProductCard, {ids: ''})
-    getProductData(){
+    handleLoad(){
+        //get cookies and count the number of products
+        var productsCountedMap = this.dublicateCounter(this.readCookie());
+        this.getProductDetailData(productsCountedMap);
     }
 
     readCookie(){
@@ -42,10 +34,90 @@ export default class Cart extends LightningElement {
         return returnArr;
     }
 
+    getProductDetailData(productsCountedMap){
+        var ids = [];
+        for(let key of productsCountedMap.keys()){
+            ids.push(key);
+        }
+        getDataForCart({ids: ids})
+            .then(result => {
+                this.products = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+        console.log(this.products.length);
+        
+    }
 
+    // @track cookiesArr;
+    // @track products; //for product detail data
+    // @track error;
+    // ids;
+    // productsCounter;
 
+    // //получили ids
+    // //нужно остаивть только уникальныке ids и заполнить кол-во id
 
+    // constructor(){
+    //     super();
+    //     this.cookiesArr = this.readCookie();
+    //     this.ids = [];
+    //     this.productsCounter = new Map();
+    //     this.products = [];
 
+    // }
+    
+    // showCartWindow(){
+    //     this.handleLoad();
+    //     var data = this.template.querySelector(".slds-is-open");
+    //     if (data){
+    //         this.template.querySelector(".slds-dropdown-trigger").classList.remove('slds-is-open');
+    //     }else{
+    //         this.template.querySelector(".slds-dropdown-trigger").classList.add('slds-is-open');
+    //     }
+         
+    // }
+
+    // handleLoad() {
+    //     this.productsCounter = this.dublicateCounter();
+    //     var productsIds = [];
+    //     for(let key of this.productsCounter.keys()){
+    //         productsIds.push(key);
+    //     }
+    //     getDataForCart({ids: productsIds})
+    //         .then(result => {
+    //             this.products = result;
+    //         })
+    //         .catch(error => {
+    //             this.error = error;
+    //         });
+    //     console.log('products ' + this.products);
+    // }
+
+    // // @wire(getDataForCart, {ids: this.cookiesArr})
+    // // products;
+
+    // readCookie(){
+    //     var arr =  document.cookie.split(';');
+    //     var returnArr = [];
+    //     arr.forEach(element => {
+    //         var value = element.split('=');
+    //         if (value.shift().trim() == 'ids'){
+    //             returnArr = JSON.parse(value);
+    //             return;
+    //         }
+    //     });
+    //     return returnArr;
+    // }
+
+    dublicateCounter(arr){
+        var productsMap = new Map();
+        arr.forEach(function(element){
+            productsMap.set(element, productsMap.get(element) + 1 || 1);
+        });
+        return productsMap;
+    }
 
     // updateCounter(event){
     //     console.log(event.detail);
