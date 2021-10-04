5a2920b37474b10690f311ecfe49717ac80a184c
Michael Leshchenko <michael.leshchenko@vrpconsulting.com>
2021-10-04 18:34:08 +0300

    updated add to cart button component

--- force-app/main/default/lwc/addToCartButton/addToCartButton.js
+++ force-app/main/default/lwc/addToCartButton/addToCartButton.js
@@ -2,31 +2,67 @@ import MailingPostalCode from '@salesforce/schema/Contact.MailingPostalCode';
 import { LightningElement, api, track } from 'lwc';
 
 export default class AddToCartButton extends LightningElement {
-    @api productid;
     @api name;
-    @api price;
+    @api productid;
     @api image;
-
+    @api price;
+    @api positive
+    @api label;
+    
     handleAddToCart(){
         var cookiesMap = new Map();
         var productMap = new Map();
+        console.log(this.positive);
 
         productMap['id'] =  this.productid;
         productMap['name'] = this.name;
         productMap['price'] = this.price;
         productMap['image'] = this.image;
+        productMap['calculatedPrice'] = productMap['price'];
         productMap['count'] = 1;
       
         cookiesMap = this.readCookie();
+        console.log(this.positive);
 
-        if(cookiesMap[this.productid]){
-            cookiesMap[this.productid]['count'] += 1
+        if (this.positive){
+            this.handlePositive(cookiesMap, productMap);
         }else{
-            cookiesMap[this.productid] = productMap;
+            this.handleNegative(cookiesMap, productMap);
         }
         document.cookie="cart=" + this.arrToJson(cookiesMap);
         console.log(document.cookie);
     }
+
+    handlePositive(cookiesMap, productMap){
+        if(cookiesMap[this.productid]){
+            cookiesMap[this.productid]['count'] += 1;
+            cookiesMap[this.productid]['calculatedPrice'] = parseFloat(cookiesMap[this.productid]['price'])
+                 * parseFloat(cookiesMap[this.productid]['count']);
+        }else{
+            cookiesMap[this.productid] = productMap; 
+        }
+    }
+
+    handleNegative(cookiesMap, productMap){
+        if(cookiesMap[this.productid]){
+            if(cookiesMap[this.productid]['count'] > 0){
+                cookiesMap[this.productid]['count'] -= 1;
+                if(cookiesMap[this.productid]['count'] == 0){
+                    // cookiesMap[this.productid] = '';
+                    delete cookiesMap[this.productid];
+                }else{
+                    cookiesMap[this.productid]['calculatedPrice'] = 
+                   parseFloat(cookiesMap[this.productid]['price'])
+                   * parseFloat(cookiesMap[this.productid]['count']); 
+                }  
+            }else{
+                console.log('deleting', productMap);
+                delete cookiesMap[this.productid];
+                console.log('deleting2', productMap);
+            }
+        }
+    }
+
 
     arrToJson(map){
         return JSON.stringify(map)
@@ -38,7 +74,7 @@ export default class AddToCartButton extends LightningElement {
         arr.forEach(element => {
             var value = element.split('=');
             if (value.shift().trim() == 'cart'){
-                if (!value == ''){
+                if (value != ''){
                     returnArr = JSON.parse(value);    
                     return;
                 }
