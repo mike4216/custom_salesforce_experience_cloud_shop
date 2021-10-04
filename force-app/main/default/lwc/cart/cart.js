7eb439aeaad9428149f2127bb97a05c74a6e33c5
Michael Leshchenko <michael.leshchenko@vrpconsulting.com>
2021-10-04 19:05:00 +0300

    formatted cart html + added cart empty text

--- force-app/main/default/lwc/cart/cart.js
+++ force-app/main/default/lwc/cart/cart.js
@@ -1,12 +1,11 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import getDataForCart from '@salesforce/apex/ShopController.getDataForCart';
-import ContactMobile from '@salesforce/schema/Case.ContactMobile';
-import { getListUi } from 'lightning/uiListApi';
 
 
 export default class Cart extends LightningElement {
     @track products;
-    @track productToShow; 
+    @track productToShow;
+    @track cartEmpty; 
     
     constructor(){
         super();
@@ -28,10 +27,7 @@ export default class Cart extends LightningElement {
     }
 
     handleLoad(){
-        console.log('dlkdlfkdk');
-        // var pr  = new Map();
         var pr = this.readCookie();
-        // console.log(pr);
        return pr;
     }
 
@@ -41,8 +37,13 @@ export default class Cart extends LightningElement {
         Object.keys(this.productToShow).forEach(el=>{
             this.products.push(this.productToShow[el]);
         })
-        console.log(this.products);
-        // console.log(this.productToShow.keys());
+        console.log('lenth', this.products.length);
+        if(this.products.length > 0){
+            this.cartEmpty = false;
+        }else{
+            this.cartEmpty = true;
+        }
+        console.log(this.cartEmpty);
     }
 
     readCookie(){
@@ -79,86 +80,4 @@ export default class Cart extends LightningElement {
         this.productToShow = this.handleLoad();
         this.handleProxyObject(); 
     }
-
-    remove(){
-        this.number--;
-        this.calculatePrice()
-    }
-
-    calculatePrice(){
-        return this.price * this.numer;
-    }
-
-    // @track cookiesArr;
-    // @track products; //for product detail data
-    // @track error;
-    // ids;
-    // productsCounter;
-
-    // //получили ids
-    // //нужно остаивть только уникальныке ids и заполнить кол-во id
-
-    // constructor(){
-    //     super();
-    //     this.cookiesArr = this.readCookie();
-    //     this.ids = [];
-    //     this.productsCounter = new Map();
-    //     this.products = [];
-
-    // }
-    
-    // showCartWindow(){
-    //     this.handleLoad();
-    //     var data = this.template.querySelector(".slds-is-open");
-    //     if (data){
-    //         this.template.querySelector(".slds-dropdown-trigger").classList.remove('slds-is-open');
-    //     }else{
-    //         this.template.querySelector(".slds-dropdown-trigger").classList.add('slds-is-open');
-    //     }
-         
-    // }
-
-    // handleLoad() {
-    //     this.productsCounter = this.dublicateCounter();
-    //     var productsIds = [];
-    //     for(let key of this.productsCounter.keys()){
-    //         productsIds.push(key);
-    //     }
-    //     getDataForCart({ids: productsIds})
-    //         .then(result => {
-    //             this.products = result;
-    //         })
-    //         .catch(error => {
-    //             this.error = error;
-    //         });
-    //     console.log('products ' + this.products);
-    // }
-
-    // // @wire(getDataForCart, {ids: this.cookiesArr})
-    // // products;
-
-    // readCookie(){
-    //     var arr =  document.cookie.split(';');
-    //     var returnArr = [];
-    //     arr.forEach(element => {
-    //         var value = element.split('=');
-    //         if (value.shift().trim() == 'ids'){
-    //             returnArr = JSON.parse(value);
-    //             return;
-    //         }
-    //     });
-    //     return returnArr;
-    // }
-
-    dublicateCounter(arr){
-        var productsMap = new Map();
-        arr.forEach(function(element){
-            productsMap.set(element, productsMap.get(element) + 1 || 1);
-        });
-        return productsMap;
-    }
-
-    // updateCounter(event){
-    //     console.log(event.detail);
-    // }
 }
